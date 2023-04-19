d2ccdcf8ef162365f1f0eb55813a3e9eb2d8a827
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-19 21:27:01 +0000

    Display product list and details

--- products/views.py
+++ products/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def products(request):
-    """ A view to return the products page """
+    """ A view to return the products page including sorting and searching """
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
