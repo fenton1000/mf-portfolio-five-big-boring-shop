fafd2236e92f14f6955c36b8e46f09577f65034d
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-20 11:08:42 +0000

    Add search and filter features

--- products/views.py
+++ products/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 def products(request):
     """ A view to return the products page including sorting and searching """
 
     products = Product.objects.all()
+    query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            search_category = request.GET['category']
+            products = products.filter(category__name=search_category)
+            category = get_object_or_404(Category, name=search_category)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!"
+                )
+                return redirect(reverse('products'))
+
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query) |
+                Q(category__friendly_name__icontains=query)
+            )
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_category': category,
     }
 
     return render(request, 'products/products.html', context)
