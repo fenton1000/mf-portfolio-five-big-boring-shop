b4f16ed3ceb23d1f9004a295a4603561409750e1
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-21 15:24:27 +0000

    Display cart contents

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,11 +12,14 @@ def cart_contents(request):
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        sub_total = 0
+        sub_total = quantity * product.price
+        total += sub_total
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
+            'sub_total': sub_total,
             'product': product,
         })
 
