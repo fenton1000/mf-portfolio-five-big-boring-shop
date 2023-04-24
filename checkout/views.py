0b9fe75aa7ca3001f71e2eb2c8c750ea1a4f5e55
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-24 12:23:03 +0000

    Add stripe_elements.js and checkout.css

--- checkout/views.py
+++ checkout/views.py
@@ -12,8 +12,16 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+    stripe_public_key = (
+        'pk_test_51N0Mk3AcCvg3bp3PZzSy'
+        'TC34W67yYOqCAkUoK10lTDe8R9ChHR'
+        'ZL1ZP7YXZ0FbCjtLKnf7r2eLqBE'
+        'XjkiiSbtqM600DGjYYt1w'
+    )
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
