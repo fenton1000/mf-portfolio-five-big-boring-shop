c2c5d01789af36bf54ca13b3256c7540f4a90a8b
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-26 14:03:36 +0000

    Allow completion of profile update via webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -90,6 +91,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     eircode=shipping_details.address.postal_code,
