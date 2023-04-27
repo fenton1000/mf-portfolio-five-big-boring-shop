39fc2be045bc0bbb878f4f785c0a4e326a8a126c
fenton1000 <fenton1000@yahoo.co.uk>
2023-04-27 11:03:11 +0000

    Hide secret key and set debug for production

--- big_boring_shop/settings.py
+++ big_boring_shop/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-uphzsbsh-)8-z+18)u@c#j)*9dbd79u^9pl@q0xcq&phh!q2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mf-pp5-big-boring-shop.herokuapp.com', 'localhost']
 
