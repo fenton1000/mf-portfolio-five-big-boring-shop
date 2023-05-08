84ce51cc6a7abbfb0d0d83e4bcbfc54477b98258
fenton1000 <fenton1000@yahoo.co.uk>
2023-05-08 20:29:03 +0000

    Add view to handle mailchimp redirect

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def thanks(request):
+    """
+    A view to return the index page with a
+    mailchimp subscription confirmation message
+    """
+
+    messages.success(request, 'Thank you for subscribing!')
+
+    return redirect('home')
